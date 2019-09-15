26c0d2438aea527a23749a410e85dd8ecab60ff3
John Ouellet <john@thinktandem.io>
2019-09-15 09:35:57 -0400

    Convert to 1.x vuepress and add in seo stuffs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,10 +2,31 @@ module.exports = {
   title: 'VuePress Template',
   description: 'Stuff and things and other stuff',
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }],
-    ['link', { rel: 'stylesheet', href: '/styles/overrides.css'}],
+    ['link', {rel: 'icon', href: '/favicon.ico'}],
+    ['link', {rel: 'stylesheet', href: '/styles/overrides.css'}],
   ],
-  ga: 'UA-xxxxxxx',
+  plugins: {
+    '@vuepress/google-analytics': {
+      ga: 'UA-xxxxxxx',
+    },
+    'autometa': {
+      site: {
+        name: 'VuePress Template',
+        twitter: 'changeme',
+      },
+      canonical_base: 'https://template-vuepress.lndo.site',
+    },
+    'canonical': {
+      baseURL: 'https://template-vuepress.lndo.site',
+    },
+    'robots': {
+      host: 'https://template-vuepress.lndo.site',
+    },
+    'sitemap': {
+      hostname: 'https://template-vuepress.lndo.site',
+      exclude: ['/404.html'],
+    },
+  },
   themeConfig: {
     repo: 'thinktandem/template-vuepress',
     repoLabel: 'GitHub',
@@ -14,8 +35,8 @@ module.exports = {
     editLinks: true,
     editLinkText: 'Edit Link Text',
     nav: [
-      { text: 'Test Directory', link: '/test-dir/' },
-      { text: 'Tandem Website', link: 'https://thinktandem.io' },
+      {text: 'Test Directory', link: '/test-dir/'},
+      {text: 'Tandem Website', link: 'https://thinktandem.io'},
     ],
     sidebar: {
       '/test-dir/': [
